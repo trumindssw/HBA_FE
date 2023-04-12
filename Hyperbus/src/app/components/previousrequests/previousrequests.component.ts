e98b95ff57bbffe1d30db372675dcf88c9015300
KhushiGupta2207 <Khushi.Gupta@truminds.com>
2023-04-12 12:22:32 +0530

    Sign in screen, Upload screen, Previous request screen

--- Hyperbus/src/app/components/previousrequests/previousrequests.component.ts
+++ Hyperbus/src/app/components/previousrequests/previousrequests.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-previousrequests',
   templateUrl: './previousrequests.component.html',
   styleUrls: ['./previousrequests.component.css']
 })
-export class PreviousrequestsComponent {
 
+export class PreviousrequestsComponent implements OnInit {
+  constructor(private router: Router) { }
+  ngOnInit(){
+      
+  }
+  onClick(){
+    //upload
+    this.router.navigate(['/upload']);
+  }
 }
