7757f3ed9aa41370fc1b9bd86054057b05b1198c
suma-shreya <suma.shreya@truminds.com>
2023-04-19 19:46:04 +0530

    drag box size shortened

--- Hyperbus/src/app/components/upload/upload.component.ts
+++ Hyperbus/src/app/components/upload/upload.component.ts
@@ -39,12 +39,10 @@ export class UploadComponent implements OnInit {
         if(this.files && this.files.length>0) {
           this.files.map((f: { createdAt: string | number | Date; }) => {
             let dt = new Date(f.createdAt);
-            console.log(dt)
             let month = dt.toLocaleString('default', { month: 'long' });
             let yr = dt.getFullYear();
             let date = dt.getDate();
             let time = dt.getHours() + "." + dt.getMinutes();
-            console.log(month, date, yr, time )
             f.createdAt = month + " " + date + ", " + yr + " " + time;
           })
         }
